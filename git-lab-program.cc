1f2aeae89446ef11f438127950c27229a5643927
Alyssa Vandale <av117818@ohio.edu>
2020-01-21 19:36:05 -0500

    Git-lab-2 stuff

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Alyssa Vandale
+ *        Date: 21 Jan. 2020
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout <<  "Hello Git!"  << endl;
+    return 0;
+}// main
